300ee8623277714f8713f0869af608a425c3ba0b
adamocallaghan <adam.ocallaghan@yahoo.ie>
2024-10-09 22:24:47 +0100

    added OptionsBuilder.t.sol to easily build compose options for commands

---
+++ test/OptionsBuilder.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.20;
+
+import "forge-std/Test.sol";
+import {console2} from "forge-std/Test.sol";
+import {OptionsBuilder} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/libs/OptionsBuilder.sol";
+
+contract StableEngineTest is Test {
+    using OptionsBuilder for bytes;
+
+    function setUp() external {}
+
+    // lZ Compose: A - B1 - B2
+    function test_Build_LzCompose_Options() public pure {
+        bytes memory options =
+            OptionsBuilder.newOptions().addExecutorLzReceiveOption(700000, 0).addExecutorLzComposeOption(0, 700000, 0);
+        console2.log("Lz Compose Message Options: ", vm.toString(options));
+    }
+}
